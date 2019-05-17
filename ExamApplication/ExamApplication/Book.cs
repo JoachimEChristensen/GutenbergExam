afa9c1571a4b60940fdaa249c4b55006a0606fc7
Joachim E Christensen <JoachimEChristensen@gmail.com>
2019-05-17 16:51:13 +0200

    Started making application for the project.

---
+++ ExamApplication/ExamApplication/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamApplication
+{
+    public class Book
+    {
+        private string name;
+        private string author;
+        public Book(string newName, string newAuthor)
+        {
+            name = newName;
+            author = newAuthor;
+        }
+    }
+}
---
+++ ExamApplication/ExamApplication/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamApplication
+{
+    public class City
+    {
+        private string name;
+        private string location;
+        public City(string newName, string newLocation)
+        {
+            name = newName;
+            location = newLocation;
+        }
+    }
+}
