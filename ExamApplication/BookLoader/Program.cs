a756a5d929e572c1111153d86a417fc14f05122f
Christian Philip Ege Nielsen <kagejohn@kagejohn.dk>
2019-05-25 13:46:12 +0200

    Start of book loader and changes needed to database scripts

---
+++ ExamApplication/BookLoader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace BookLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Desktop\kage\Downloads\archive\root\zipfiles\1jcfs10.txt";
+            string readText = File.ReadAllText(path);
+            //Console.WriteLine(readText);
+        }
+    }
+}
